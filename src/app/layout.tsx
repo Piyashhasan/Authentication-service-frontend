f6887743f10e4d9c7652fbdec51dc9f3d65c3df9
Piyashhasan <piyashhasan38@gmail.com>
2026-07-11 20:28:39 +0600

    fix: Hydration warning resolve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,14 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning={true}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        suppressHydrationWarning={true}
+        className="min-h-full flex flex-col"
+      >
+        {children}
+      </body>
     </html>
   );
 }
